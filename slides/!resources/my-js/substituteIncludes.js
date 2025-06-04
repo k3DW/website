cd21d8a911ca9d88acd9e1df0a25d516c22ffe68
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-06-04 00:09:08 -0500

    C++ on Sea 2025 slides first draft

--- slides/!resources/my-js/substituteIncludes.js
+++ slides/!resources/my-js/substituteIncludes.js
@@ -1,10 +1,15 @@
 export async function substituteIncludes() {
-    const includes = [...document.getElementsByTagName("include")];
-    for (const include of includes) {
-        const filePath = include.getAttribute("src");
-        const file = await fetch(filePath);
-        const content = await file.text();
-        include.insertAdjacentHTML("afterend", content);
-        include.remove();
+    while (true) {
+        const includes = [...document.getElementsByTagName("include")];
+        if (includes.length == 0) {
+            break;
+        }
+        for (const include of includes) {
+            const filePath = include.getAttribute("src");
+            const file = await fetch(filePath);
+            const content = await file.text();
+            include.insertAdjacentHTML("afterend", content);
+            include.remove();
+        }
     }
 }
